f2e93d565d35285099077e415058087765726141
SevenMay <xiaobin_zhen@163.com>
2015-06-30 11:14:52 +0800

    Initial Commit

---
+++ Scanning/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Scanning
+//
+//  Created by tsmc on 15/6/30.
+//  Copyright (c) 2015年 SevenMay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ Scanning/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  Scanning
+//
+//  Created by tsmc on 15/6/30.
+//  Copyright (c) 2015年 SevenMay. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
